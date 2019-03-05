5f974cbd8d0b601ddc34b6d049049c105df016cb
Yuriy Belenko <yura-bely@mail.ru>
2019-03-05 06:14:48 +0500

    Add user creation to profiles import

--- public/extensions/custom/imports/ProfilesImport.php
+++ public/extensions/custom/imports/ProfilesImport.php
@@ -2,16 +2,92 @@
 
 namespace Directus\Custom\Imports;
 
+use Directus\Services\UsersService;
+use Directus\Services\ItemsService;
+use Directus\Application\Container;
+use Directus\Validator\Exception\InvalidRequestException;
+use Directus\Database\Schema\SchemaManager;
+use Directus\Util\StringUtils;
+use Directus\Database\Exception\DuplicateItemException;
+use PASSWORD_BCRYPT;
+
 class ProfilesImport
 {
 
-    public function __construct()
-    {
+    /* @var Container App container */
+    protected $container;
 
+    /**
+     * @param Container $container
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
     }
 
     public function execute(array $data)
     {
+        $userService = new UsersService($this->container);
+        $itemsService = new ItemsService($this->container);
+        $rejected = 0;
 
+        $customersRole = $itemsService->findOne(SchemaManager::COLLECTION_ROLES, [
+            'filter' => [
+                'name' => 'Customers',
+            ],
+        ]);
+
+        foreach ($data as $item) {
+            $item = $this->stripPayloadStrings($item);
+            $password = StringUtils::random();
+            $hashed = password_hash($password, PASSWORD_BCRYPT, ['cost' => 4]);
+
+            $payload = [
+                'status' => 'invited',
+                'first_name' => $item['fullname'],
+                'last_name' => $item['fullname'],
+                'email' => $item['email'],
+                'password' => $hashed,
+                'timezone' => 'Asia/Jerusalem',
+                'email_notifications' => 0,
+            ];
+            try {
+                $user = $userService->create($payload);
+
+                $payload = [
+                    'user' => $user['data']['id'],
+                    'role' => $customersRole['data']['id'],
+                ];
+
+                $itemsService->createItem(SchemaManager::COLLECTION_USER_ROLES, $payload);
+            } catch (InvalidRequestException $ex) {
+                $rejected++;
+            } catch (DuplicateItemException $ex) {
+                $rejected++;
+            }
+        }
+    }
+
+    protected function stripPayloadStrings(array $payload)
+    {
+        if (!is_array($payload)) return $payload;
+
+        foreach ($payload as $key => $value) {
+            $payload[$key] = $this->stripString($value);
+        }
+        return $payload;
+    }
+
+    protected function stripString($value)
+    {
+        if(!is_string($value)) return $value;
+
+        // remove invisible chars
+        $value = preg_replace('/[\x00-\x1F\x7F\xA0]/u', '', $value);
+
+        // remove rtl and other special chars
+        $value = preg_replace('/(\x{200e}|\x{200f})/u', '', $value);
+
+        return trim($value);
     }
 }
--- tests/extensions/custom/database/DatabaseTestCase.php
+++ tests/extensions/custom/database/DatabaseTestCase.php
@@ -6,6 +6,11 @@ use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 use PHPUnit\DbUnit\Database\Connection;
 use PHPUnit\DbUnit\DataSet\IDataSet;
+use Directus\Application\Application;
+use Directus\Database\SchemaService;
+use Directus\Database\TableGateway\BaseTableGateway;
+use Directus\Database\TableGatewayFactory;
+use Directus\Permissions\Acl;
 use PDO;
 
 abstract class DatabaseTestCase extends TestCase
@@ -18,6 +23,34 @@ abstract class DatabaseTestCase extends TestCase
     // only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
     private $conn = null;
 
+    static protected $container = null;
+
+    static public function setupBeforeClass() {
+        parent::setupBeforeClass();
+
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
+        if (self::$container === null) {
+            $app = new Application(__DIR__, self::getAppConfig());
+            $container = $app->getContainer();
+            self::$container = $container;
+
+            $acl = new Acl([
+                'directus_users' => [[
+                    'role' => 1,
+                ]],
+            ]);
+            $acl->setUserId(1);
+            $container->set('acl', $acl);
+            SchemaService::setAclInstance($acl);
+            SchemaService::setConnectionInstance($container->get('database'));
+            SchemaService::setConfig($container->get('config'));
+            BaseTableGateway::setHookEmitter($container->get('hook_emitter'));
+            BaseTableGateway::setContainer($container);
+            TableGatewayFactory::setContainer($container);
+        }
+    }
+
     /**
      * @return Connection
      */
@@ -25,11 +58,69 @@ abstract class DatabaseTestCase extends TestCase
     {
         if ($this->conn === null) {
             if (self::$pdo == null) {
-                self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+                self::$pdo = new PDO(
+                    "mysql:dbname={$GLOBALS['DB_DBNAME']};host={$GLOBALS['DB_HOST']};port={$GLOBALS['DB_PORT']}",
+                    $GLOBALS['DB_USER'],
+                    $GLOBALS['DB_PASSWD']
+                );
             }
             $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
         }
 
         return $this->conn;
+    }
+
+    final static protected function getAppConfig()
+    {
+        $config = [
+            'app' => [
+                'env' => 'development',
+                'timezone' => 'America/New_York',
+            ],
+
+            'database' => [
+                'type' => 'mysql',
+                'host' => $GLOBALS['DB_HOST'],
+                'port' => $GLOBALS['DB_PORT'],
+                'name' => $GLOBALS['DB_DBNAME'],
+                'username' => $GLOBALS['DB_USER'],
+                'password' => $GLOBALS['DB_PASSWD'],
+                'engine' => 'InnoDB',
+                'charset' => 'utf8mb4',
+                // When using unix socket to connect to the database the host attribute should be removed
+                'socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+                // 'socket' => '',
+            ],
+
+            'cache' => [
+                'enabled' => false,
+                'response_ttl' => 3600, // seconds
+            ],
+
+            'storage' => [
+                'adapter' => 'local',
+                // The storage root is the directus root directory.
+                // All path are relative to the storage root when the path is not starting with a forward slash.
+                // By default the uploads directory is located at the directus public root
+                // An absolute path can be used as alternative.
+                'root' => 'public/uploads/_/originals',
+                // This is the url where all the media will be pointing to
+                // here is where Directus will assume all assets will be accessed
+                // Ex: (yourdomain)/uploads/_/originals
+                'root_url' => '/uploads/_/originals',
+                // Same as "root", but for the thumbnails
+                'thumb_root' => 'public/uploads/_/thumbnails',
+            ],
+
+            'mail' => [
+                'default' => [
+                    'transport' => 'sendmail',
+                    'from' => 'admin@example.com'
+                ],
+            ],
+        ];
+
+
+        return $config;
     }
 }
