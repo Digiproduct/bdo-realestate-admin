913973bc8b85b4909ebc3105f4963637d6be9d91
Yuriy Belenko <yura-bely@mail.ru>
2019-03-09 06:28:17 +0500

    Put test fixtures in separated files

--- tests/extensions/custom/imports/ConstructionProgressImportTest.php
+++ tests/extensions/custom/imports/ConstructionProgressImportTest.php
@@ -76,70 +76,10 @@ class ConstructionProgressImportTest extends DatabaseTestCase
 
     public function provideConstructionProgressData()
     {
+        $constructionProgressFixture = require(__DIR__ . '/fixtures/construction_progress.inc.php');
         return [
             [
-                [
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'נובמבר 2013 - זכיה במכרז',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'ינואר 2014- הסכם שיתוף',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'אוקטובר 2014- תכנון ובקשת היתר',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'ינואר 2015- הכנת דוח אפס',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'אוקטובר 2015 - ליווי בנקאי',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'ינואר 2016- התחלת בניה',
-                        'is_complete' => true,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'דצמבר 2016- סיום שלד',
-                        'is_complete' => false,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'יולי 2017- סיום מערכות בניין',
-                        'is_complete' => false,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'דצמבר 2017- גימורים ופיתוח',
-                        'is_complete' => false,
-                    ],
-                    [
-                        'updated_date' => '2019-01-08',
-                        'group_name' => 'קבוצת אחיסמך',
-                        'milestone_name' => 'פברואר 2018- קבלת מפתחות',
-                        'is_complete' => false,
-                    ],
-                ],
+                $constructionProgressFixture,
             ],
         ];
     }
